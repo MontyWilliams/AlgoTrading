8b6d5b50afb16425434eb3a3b36ebb6f972b9056
Monty Williams <Roddrickwilliams123@gmail.com>
2026-03-26 08:17:40 -0900

    will be tracking liquidations across multiple exchanges

--- liquidations.py
+++ liquidations.py
@@ -6,12 +6,5 @@ import pytz
 from websockets import connect
 from termcolor import cprint
 
-
-websocket_url = 'wss://fstream.binance.com/ws/!forceOrder@arr'
-filename = 'binance_liq.csv'
-
-if not os.path.isfile(filename):
-    with open(filename, 'w') as f:
-        f.write(",".join([
-            'symbol', 'side', 'order_type', 'time_in_force', 'original_quantity', 'price', 'average_price', 'order_status', 'order_last_filled_quantity', 'order_filled_accumulated_quantity', 'order_trade_time', 'usd_size'
-        ]) + "\n")
+"""Track Liquidations on multiple exchanges to get the best picture of the market.
+"""
